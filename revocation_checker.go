0074373b5795ececb0d6bd7da24b41d2ed02bfb3
debug-evac <122994921+debug-evac@users.noreply.github.com>
2026-02-07 02:26:40 +0100

    corrected behavior of 'mode' config option: crl or ocsp is now preferred and ocsp or crl is not used depending on the option used

--- revocation_checker.go
+++ revocation_checker.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 
 	"github.com/caddyserver/caddy/v2"
+	"github.com/gr33nbl00d/caddy-revocation-validator/config"
+	"github.com/gr33nbl00d/caddy-revocation-validator/core"
 	"github.com/gr33nbl00d/caddy-revocation-validator/crl"
 	"github.com/gr33nbl00d/caddy-revocation-validator/ocsp"
 	"go.uber.org/zap"
@@ -14,6 +16,7 @@ type RevocationChecker struct {
 	RevocationConfig      *ParsedRevocationConfig
 	crlRevocationChecker  *crl.CRLRevocationChecker
 	ocspRevocationChecker *ocsp.OCSPRevocationChecker
+	checkOrder            [](func(*x509.Certificate, [][]*x509.Certificate) (*core.RevocationStatus, error))
 }
 
 func (c *RevocationChecker) Provision(ctx caddy.Context, logger *zap.Logger, revocationConfig *ParsedRevocationConfig) error {
@@ -33,31 +36,34 @@ func (c *RevocationChecker) Provision(ctx caddy.Context, logger *zap.Logger, rev
 		ocsp_err = c.ocspRevocationChecker.Provision(revocationConfig.OCSPConfigParsed, logger)
 	}
 
+	switch c.RevocationConfig.ModeParsed {
+	case config.RevocationCheckModePreferOCSP:
+		c.checkOrder = append(c.checkOrder, c.ocspRevocationChecker.IsRevoked, c.crlRevocationChecker.IsRevoked)
+	case config.RevocationCheckModePreferCRL:
+		c.checkOrder = append(c.checkOrder, c.crlRevocationChecker.IsRevoked, c.ocspRevocationChecker.IsRevoked)
+	case config.RevocationCheckModeCRLOnly:
+		c.checkOrder = append(c.checkOrder, c.crlRevocationChecker.IsRevoked)
+	case config.RevocationCheckModeOCSPOnly:
+		c.checkOrder = append(c.checkOrder, c.ocspRevocationChecker.IsRevoked)
+	case config.RevocationCheckModeDisabled:
+	}
+
 	return errors.Join(crl_err, ocsp_err)
 }
 
 func (c *RevocationChecker) VerifyClientCertificate(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
 	if len(verifiedChains) > 0 {
 		clientCertificate := verifiedChains[0][0]
-		if c.RevocationConfig.IsOCSPCheckingEnabled() {
-			revoked, err := c.ocspRevocationChecker.IsRevoked(clientCertificate, verifiedChains)
-			if err != nil {
-				return err
-			}
-			if revoked.Revoked {
-				return errors.New("client certificate was revoked")
-			}
-		}
-		if c.RevocationConfig.IsCRLCheckingEnabled() {
-			revoked, err := c.crlRevocationChecker.IsRevoked(clientCertificate, verifiedChains)
-			if err != nil {
-				return err
-			}
-			if revoked.Revoked {
-				return errors.New("client certificate was revoked")
-			}
-		}
 
+		for i := range len(c.checkOrder) {
+			revoked, err := c.checkOrder[i](clientCertificate, verifiedChains)
+			if err != nil {
+				return err
+			}
+			if revoked.Revoked {
+				return errors.New("client certificate was revoked")
+			}
+		}
 	}
 	return nil
 }
